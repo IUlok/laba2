b9bbea08c144757ee85b3993ce68af2f340860da
fish <99109745+IUlok@users.noreply.github.com>
2022-05-31 12:14:58 +0300

    Update makros.asm

--- makros.asm
+++ makros.asm
@@ -3,30 +3,38 @@ include io.asm
 .stack 100
 .data
   array db 100 dup(?)
+  msg db "Enter array: ", 0Ah, 0Dh, '$'
+  msg1 db "Enter size of array: ", 0Ah, 0Dh, '$'
 .code
-DelProbel MACRO count, array
-      xor si, si
-      mov cx, bx
-starting:
-      mov ah,01h
-      int 21h
-      cmp al,' '
-      jne ifelse
-      jmp cycle
-ifelse:
-      mov array[si],al
-      inc si
-cycle:
-      loop starting
-      mov array[si], '$'
-      newline
-endm
-.startup
-start:
-      InInt bx
-      DelProbel bx, array
-      mov ah, 9
-      lea dx, array
-      int 21h
-      .exit 0
-end
+DelProbel MACRO count, array    ; Макроопределение
+      xor si, si                ; Обнуление счетчика si
+      mov cx, bx                ; Помещение длины строки в cx
+starting:                       ; Метка starting
+      mov ah,01h                ;]
+      int 21h                   ;] - Ввод символа с клавиатуры
+      cmp al,' '                ; Сравнение символа с пробелом
+      jne ifelse                ; Если не пробел - переход на метку ifelse
+      jmp cycle                 ; Иначе (т.е. если пробел) - переход на метку cycle
+ifelse:                         ; Метка ifelse
+      mov array[si],al          ; Запись текущего символа в массив по индексу текущего значения si
+      inc si                    ; Увеличение счетчика на 1
+cycle:                          ; Метка cycle
+      loop starting             ; Новая итерация цикла
+      mov array[si], '$'        ; Запись нулевого байта в конец строки
+      newline                   ; Перенос строки
+endm                            ; Конец макроса
+.startup                        ;]
+start:                          ;] - Начало программы
+      mov ah, 9                 ;]
+      lea dx, msg1              ;] - Вывод на экран msg1
+      int 21h                   ;]
+      InInt bx                  ; Ввод длины строки
+      mov ah, 9                 ;]
+      lea dx, msg               ;] - Вывод на экран msg
+      int 21h                   ;]
+      DelProbel bx, array       ; Вызов макроса
+      mov ah, 9                 ;]
+      lea dx, array             ;] - Вывод на экран строки
+      int 21h                   ;]
+      .exit 0                   ;]
+end                             ;] - Конец программы
